91bc36b795399d7822f152bb5c689c5d3e46ba43
atenyaeves <atenyaev@protonmail.com>
2021-10-11 19:20:26 +0700

    Fix api url

--- app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/di/Module.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/di/Module.kt
@@ -6,7 +6,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "api.openweathermap.org/data/2.5/weather"
+const val BASE_URL = "api.openweathermap.org/data/2.5"
 
 val appModule = module {
     single {
