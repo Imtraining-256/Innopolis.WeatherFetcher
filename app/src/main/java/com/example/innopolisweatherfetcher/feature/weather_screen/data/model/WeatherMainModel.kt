e31d2788303ae0901a4dddcb61b61215adfacad5
atenyaeves <atenyaev@protonmail.com>
2021-10-11 23:14:45 +0700

    Fix code style

--- app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/data/model/WeatherMainModel.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/data/model/WeatherMainModel.kt
@@ -3,7 +3,10 @@ package com.example.innopolisweatherfetcher.feature.weather_screen.data.model
 import com.google.gson.annotations.SerializedName
 
 data class WeatherMainModel(
-    @SerializedName("temp") val temp: String,
-    @SerializedName("temp_min") val tempMin: String,
-    @SerializedName("temp_max") val tempMax: String
+    @SerializedName("temp")
+    val temp: String,
+    @SerializedName("temp_min")
+    val tempMin: String,
+    @SerializedName("temp_max")
+    val tempMax: String
 )
