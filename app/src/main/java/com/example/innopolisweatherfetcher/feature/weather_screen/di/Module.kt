b3ee2c37307d293c4ab91a27af342490c693607b
atenyaeves <atenyaev@protonmail.com>
2021-10-12 01:21:18 +0700

    Reg depend

--- app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/di/Module.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/di/Module.kt
@@ -1,7 +1,13 @@
 package com.example.innopolisweatherfetcher.feature.weather_screen.di
 
 import com.example.innopolisweatherfetcher.feature.weather_screen.data.api.WeatherApi
+import com.example.innopolisweatherfetcher.feature.weather_screen.data.api.WeatherRemoteSource
+import com.example.innopolisweatherfetcher.feature.weather_screen.data.api.WeatherRepository
+import com.example.innopolisweatherfetcher.feature.weather_screen.data.api.WeatherRepositoryImpl
+import com.example.innopolisweatherfetcher.feature.weather_screen.domain.WeatherInteractor
+import com.example.innopolisweatherfetcher.feature.weather_screen.ui.WeatherScreenViewModel
 import okhttp3.OkHttpClient
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -24,5 +30,21 @@ val appModule = module {
 
     single<WeatherApi> {
         get<Retrofit>().create(WeatherApi::class.java)
+    }
+
+    single<WeatherRemoteSource> {
+        WeatherRemoteSource(get<WeatherApi>())
+    }
+
+    single<WeatherRepository> {
+        WeatherRepositoryImpl(get<WeatherRemoteSource>())
+    }
+
+    single<WeatherInteractor> {
+        WeatherInteractor(get<WeatherRepository>())
+    }
+
+    viewModel {
+        WeatherScreenViewModel(get<WeatherInteractor>())
     }
 }
