e288a6454a1c8a4ecb2ef9ebef91d17e534f1656
atenyaeves <atenyaev@protonmail.com>
2021-10-12 01:20:24 +0700

    Api suspend

--- app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/data/api/WeatherApi.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/data/api/WeatherApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("/weather")
-    fun getWeather(
+    suspend fun getWeather(
         @Query("q") cityName: String,
         @Query("appid") appId: String = "",
         @Query("units") units: String = "metric"
