21edea30c3ebb03e09920ffa2b7fb04df0c7fd32
atenyaeves <atenyaev@protonmail.com>
2021-10-11 19:20:38 +0700

    Added models

---
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/data/model/WeatherModel.kt
@@ -0,0 +1,5 @@
+package com.example.innopolisweatherfetcher.feature.weather_screen.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherModel(@SerializedName("main") val main: WeatherMainModel)
