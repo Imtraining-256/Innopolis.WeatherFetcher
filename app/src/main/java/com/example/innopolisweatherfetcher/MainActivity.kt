29d85ce790c11239c57e58758a238f6fc8d8e5c9
atenyaeves <atenyaev@protonmail.com>
2021-10-14 01:28:14 +0700

    Added fun activity

--- app/src/main/java/com/example/innopolisweatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.innopolisweatherfetcher
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.innopolisweatherfetcher.feature.weather_screen.ui.WeatherScreenActivity
 
 class MainActivity : AppCompatActivity() {
     private val presenter = MainPresenter()
@@ -9,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weatherButton = findViewById<Button>(R.id.weatherButton)
+        weatherButton.setOnClickListener {
+            Intent(this, WeatherScreenActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/ui/WeatherScreenActivity.kt
+++ app/src/main/java/com/example/innopolisweatherfetcher/feature/weather_screen/ui/WeatherScreenActivity.kt
@@ -1,8 +1,32 @@
 package com.example.innopolisweatherfetcher.feature.weather_screen.ui
 
+import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.innopolisweatherfetcher.R
+import com.example.innopolisweatherfetcher.feature.weather_screen.domain.model.WeatherDomainModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class WeatherScreenActivity : AppCompatActivity() {
     val weatherViewModel by viewModel<WeatherScreenViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        weatherViewModel.liveData.observe(this, Observer(::render))
+
+        weatherViewModel.requestWeather()
+    }
+
+    private fun render(state: WeatherDomainModel) {
+        findViewById<TextView>(R.id.tvTemperature).let {
+            it.text = state.temperature
+        }
+
+        findViewById<TextView>(R.id.tvHumidity).let {
+            it.text = state.humidity
+        }
+    }
 }
